b607551e4f814f4d243b83f0e9b1fd13978869df
rahmanezraxyz <officialdidesain@gmail.com>
2025-05-17 13:31:19 +0700

    Update aboutMeData.js

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,13 +1,12 @@
 export const aboutMeData = [
-	{
-		id: 1,
-		bio: 'I'm a passionate graphic designer with a background in Computer and Electronics Science from Universitas Gadjah Mada, Indonesia. My journey blends the precision of technology with the creativity of visual storytelling.
+  {
+    id: 1,
+    bio: `I'm a passionate graphic designer with a background in Computer and Electronics Science from Universitas Gadjah Mada, Indonesia. My journey blends the precision of technology with the creativity of visual storytelling.
 With a keen eye for design and detail, I craft impactful visuals — from branding and digital content to user-centered interfaces. I believe that good design not only looks beautiful but also communicates effectively.
-I'm always eager to explore new ideas, collaborate on creative projects, and bring visual concepts to life through thoughtful design.'
-},
-
-	{
-		id: 2,
-		bio: 'Let’s build something great together.'
-	}
+I'm always eager to explore new ideas, collaborate on creative projects, and bring visual concepts to life through thoughtful design.`
+  },
+  {
+    id: 2,
+    bio: 'Let’s build something great together.'
+  }
 ];
