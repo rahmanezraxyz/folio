4477ee907a93c7d0a6268228590ab3da903b1723
rahmanezraxyz <officialdidesain@gmail.com>
2025-05-18 02:33:39 +0700

    Add files via upload

--- src/context/SingleProjectContext.jsx
+++ src/context/SingleProjectContext.jsx
@@ -1,20 +1,41 @@
-import { useState, createContext } from 'react';
-import { singleProjectData as singleProjectDataJson } from '../data/singleProjectData';
+import { useState, createContext, useEffect } from "react";
+import { singleProjectData as defaultProjectData } from "../data/singleProjectData";
+import { projectsData } from "../data/projects";
 
 const SingleProjectContext = createContext();
 
-export const SingleProjectProvider = ({ children }) => {
-	const [singleProjectData, setSingleProjectData] = useState(
-		singleProjectDataJson
-	);
+export const SingleProjectProvider = ({ children, projectId }) => {
+  const [singleProjectData, setSingleProjectData] =
+    useState(defaultProjectData);
 
-	return (
-		<SingleProjectContext.Provider
-			value={{ singleProjectData, setSingleProjectData }}
-		>
-			{children}
-		</SingleProjectContext.Provider>
-	);
+  useEffect(() => {
+    if (projectId) {
+      const projectIdNum = parseInt(projectId, 10);
+      const project = projectsData.find((p) => p.id === projectIdNum);
+
+      if (project) {
+        const projectData = {
+          ProjectHeader:
+            project.ProjectHeader || defaultProjectData.ProjectHeader,
+          ProjectImages:
+            project.ProjectImages || defaultProjectData.ProjectImages,
+          ProjectInfo: project.ProjectInfo || defaultProjectData.ProjectInfo,
+          RelatedProject:
+            project.RelatedProject || defaultProjectData.RelatedProject,
+        };
+
+        setSingleProjectData(projectData);
+      }
+    }
+  }, [projectId]);
+
+  return (
+    <SingleProjectContext.Provider
+      value={{ singleProjectData, setSingleProjectData }}
+    >
+      {children}
+    </SingleProjectContext.Provider>
+  );
 };
 
 export default SingleProjectContext;
