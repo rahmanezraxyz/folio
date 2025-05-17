be878326b866fafb7a126b578a1a12df597044c9
rahmanezraxyz <officialdidesain@gmail.com>
2025-05-17 12:11:21 +0700

    Update aboutMeData.js

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,15 @@
 export const aboutMeData = [
 	{
 		id: 1,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
-	},
+		bio: 'I'm a passionate graphic designer with a background in Computer and Electronics Science from Universitas Gadjah Mada, Indonesia. My journey blends the precision of technology with the creativity of visual storytelling.
+
+With a keen eye for design and detail, I craft impactful visuals — from branding and digital content to user-centered interfaces. I believe that good design not only looks beautiful but also communicates effectively.
+
+I'm always eager to explore new ideas, collaborate on creative projects, and bring visual concepts to life through thoughtful design.
+},
+
 	{
 		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+		bio: 'Let’s build something great together.
 	},
 ];
