62757bd20f887a8bcd3e71c135372aa132aa67d1
rahmanezraxyz <officialdidesain@gmail.com>
2025-05-17 18:16:55 +0700

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@
 import WebImage1 from '../images/web-project-1.jpg';
 import WebImage2 from '../images/web-project-2.jpg';
 import MobileImage1 from '../images/mobile-project-1.jpg';
-import MobileImage2 from '../images/mobile-project-2.jpg';
+import MobileImage2 from '../images/ptdnp3';
 import UIImage1 from '../images/ui-project-1.jpg';
 import UIImage2 from '../images/ui-project-2.jpg';
 
