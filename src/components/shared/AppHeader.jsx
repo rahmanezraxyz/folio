1781816849cad2193039504543b1bcfacf97105e
rahmanezraxyz <officialdidesain@gmail.com>
2025-05-17 15:17:04 +0700

    Update AppHeader.jsx

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -3,7 +3,7 @@ import { FiMenu, FiMoon, FiSun, FiX } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import useThemeSwitcher from '../../hooks/useThemeSwitcher';
 import HireMeModal from '../HireMeModal';
-import logoLight from '../../images/logo-dark.png';
+import logoLight from '../../images/logo-light.png';
 import logoDark from '../../images/logo-white.png';
 import { motion } from 'framer-motion';
 import Button from '../reusable/Button';
