27057edffa9a749be078ecfde38ff1ba467784a0
rahmanezraxyz <officialdidesain@gmail.com>
2025-05-18 07:34:28 +0700

    Update ContactDetails.jsx

--- src/components/contact/ContactDetails.jsx
+++ src/components/contact/ContactDetails.jsx
@@ -3,17 +3,17 @@ import { FiPhone, FiMapPin, FiMail } from 'react-icons/fi';
 const contacts = [
 	{
 		id: 1,
-		name: 'Your Address, Your City, Your Country',
+		name: 'Kediri Regency, East Java, Indonesia',
 		icon: <FiMapPin />,
 	},
 	{
 		id: 2,
-		name: 'email@domain.com',
+		name: 'nurrohman4400@gmail.com',
 		icon: <FiMail />,
 	},
 	{
 		id: 3,
-		name: '555 8888 888',
+		name: '+62 857 0617 5365 / +62 856 0786 5194',
 		icon: <FiPhone />,
 	},
 ];
