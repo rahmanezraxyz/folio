e05bccb909940c43a994889df3b3e2753d451dce
rahmanezraxyz <officialdidesain@gmail.com>
2025-05-19 05:46:34 +0700

    Update useThemeSwitcher.jsx

--- src/hooks/useThemeSwitcher.jsx
+++ src/hooks/useThemeSwitcher.jsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from 'react';
 
 const useThemeSwitcher = () => {
-  const [theme, setTheme] = useState(() => localStorage.theme || 'light');
+const \[theme, setTheme] = useState(localStorage.theme);
+const activeTheme = theme === 'dark' ? 'light' : 'dark';
 
-  useEffect(() => {
-    const root = window.document.documentElement;
-    const oppositeTheme = theme === 'dark' ? 'light' : 'dark';
+```
+useEffect(() => {
+	const root = window.document.documentElement;
 
-    root.classList.remove(oppositeTheme);
-    root.classList.add(theme);
-    localStorage.setItem('theme', theme);
-  }, [theme]);
+	root.classList.remove(activeTheme);
+	root.classList.add(theme);
+	localStorage.setItem('theme', theme);
+}, [theme, activeTheme]);
 
-  return [theme, setTheme]; // ✅ kembalikan theme, bukan activeTheme
+return [activeTheme, setTheme];
+```
+
 };
 
 export default useThemeSwitcher;
